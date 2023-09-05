9b66da1b4b9c5c97ab11a301f8d9141526af789f
Niraj  Tiwari <tniraj7@gmail.com>
2023-09-05 20:46:33 +0900

    add ui automation test

--- HideFloatingViewWhenListScrollingDown/HideFloatingViewWhenListScrollingDownUITests/HideFloatingViewWhenListScrollingDownUITests.swift
+++ HideFloatingViewWhenListScrollingDown/HideFloatingViewWhenListScrollingDownUITests/HideFloatingViewWhenListScrollingDownUITests.swift
@@ -3,7 +3,25 @@ import XCTest
 final class HideFloatingViewWhenListScrollingDownUITests: XCTestCase {
 
     func testExample() throws {
+        
         let app = XCUIApplication()
         app.launch()
+        
+
+        let item1Element = app.scrollViews.otherElements.containing(.staticText, identifier:"Item 1").element
+        
+        item1Element.swipeUp()
+        item1Element.swipeUp()
+
+        item1Element.swipeDown()
+        item1Element.swipeDown()
+        
+        item1Element.swipeUp()
+        item1Element.swipeUp()
+        
+        item1Element.swipeDown()
+        item1Element.swipeDown()
+
+        item1Element.tap()
     }
 }
