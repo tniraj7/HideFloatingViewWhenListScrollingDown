bd0fc95ddc193f38305147c08130652ac9ec1dab
Niraj  Tiwari <tniraj7@gmail.com>
2023-09-05 20:36:01 +0900

    add UITests bundle and share the UI test scheme

---
+++ HideFloatingViewWhenListScrollingDown/HideFloatingViewWhenListScrollingDownUITests/HideFloatingViewWhenListScrollingDownUITests.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+final class HideFloatingViewWhenListScrollingDownUITests: XCTestCase {
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+}
